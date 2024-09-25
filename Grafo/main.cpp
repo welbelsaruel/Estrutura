6c95ae40e2205973db388d2efe6a443cf5feee6f
welbelsaruel <163694887+welbelsaruel@users.noreply.github.com>
2024-09-24 23:35:39 -0300

    Create main.cpp

---
+++ Grafo/main.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//#include <fstream>
+#include "Grafo.h"
+//#include <memory>
+
+using namespace std;
+
+int main(){
+    
+    Grafo g;
+    g.Cria_Matriz("/home/well/Downloads/instncias_grafo/pcv10.txt");
+      
+}
